e8f535772d0ee54970ecbeb655138f9d27b13008
RyanWalck <rwalck33@gmail.com>
2023-06-05 16:54:09 -0400

    Resettignup pages and other components

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import { Link  } from "react-router-dom";
+
+function Nav() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/breweriesIndex">Breweries Index</Link>
+        </nav>
+    );
+}
+
+export default Nav;
