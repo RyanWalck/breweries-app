d01aeee26c02f6ee7e429be4c8dbce2277ced47d
RyanWalck <rwalck33@gmail.com>
2023-06-06 21:08:19 -0400

    Spelling errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 function Home() {
   return (
     <main className="home">
-      <h1>Brewerie Buddy</h1>
+      <h1>Brewery Buddy</h1>
 
       <p>
-        Welcome to Brewerie Buddy! Here you will be able to explore Breweries
+        Welcome to Brewery Buddy! Here you will be able to explore Breweries
         and find out all that wonderful information about them. This is your one
         stop shop for any and all information about Breweries all over the
         Country!
